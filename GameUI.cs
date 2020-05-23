979e62790dc65ddf4db9822e914875d345028ae1
Niruiz <43117479+Niruiz@users.noreply.github.com>
2020-05-24 00:59:36 +0200

    score funciona
    Ya cambia el score de la pantalla

--- GameUI.cs
+++ GameUI.cs
@@ -1,18 +1,56 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameUI : MonoBehaviour
+
 {
+    public TextMeshProUGUI scoreText;
+    public GameObject endScreen;
+    public TextMeshProUGUI endScreenHeader;
+    public TextMeshProUGUI endScreenScoreText;
+
+
+    public GameObject pauseScreen;
+
+    // instance
+    public static GameUI instance;
+
     // Start is called before the first frame update
+
+    void Awake()
+    {
+        instance = this;
+    }
     void Start()
     {
-        
+        UpdateScoreText();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void UpdateScoreText()
     {
-        
+        scoreText.text = "Score: " + GameManager.instance.score;
     }
+    public void SetEndScreen(bool hasWon)
+    {
+        endScreen.SetActive(true);
+
+        endScreenScoreText.text = "<b>Score</b>\n" + GameManager.instance.score;
+
+        if (hasWon)
+        {
+            endScreenHeader.text = "You Win";
+            endScreenHeader.color = Color.green;
+        }
+        else
+        {
+            endScreenHeader.text = "Game Over";
+            endScreenHeader.color = Color.red;
+        }
+    }
+    
 }
+ 
