44f5f346b4298dc686c52bebd588075982b7019e
Niruiz <43117479+Niruiz@users.noreply.github.com>
2020-06-05 11:10:53 +0200

    Pause y score funcionan

--- GameManager.cs
+++ GameManager.cs
@@ -3,73 +3,93 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour
+
+public class PlayerController : MonoBehaviour
 {
-    public int score;
+    public float moveSpeed;
+    public float jumpForce;
+    private Rigidbody rig;
+    private AudioSource audioSource;
 
-    public bool paused;
-
-    // instance
-    public static GameManager instance;
-    
     void Awake()
     {
-        if (instance != null && instance != this)
-        {
-            Destroy(gameObject);
-        }
-        else
-        {
-            instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
+        // get the components
+        rig = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
-    // pauses or unpauses the game
-    public void TogglePauseGame()
-    {
-        paused = !paused;
 
-        if (paused)
-            Time.timeScale = 0.0f;
-        else
-            Time.timeScale = 1.0f;
-
-        GameUI.instance.TogglePauseScreen(paused);
-    }
-
-    public void AddScore(int scoreToGive)
+    void Update()
     {
-        score += scoreToGive;
-        GameUI.instance.UpdateScoreText();
-    }
+        Move();
 
-    public void ResetScore()
-    {
-        score = 0;
-        GameUI.instance.UpdateScoreText();
-    }
-    public void LevelEnd()
-    {
-        // is this the last level?
-        if (SceneManager.sceneCountInBuildSettings == SceneManager.GetActiveScene().buildIndex + 1)
+        if (Input.GetButtonDown("Jump"))
         {
-            // display the win screen
-            WinGame();
-        }
-        else
-        {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            TryJump();
         }
     }
-    
-    public void WinGame()
+
+    void Move()
     {
-        GameUI.instance.SetEndScreen(true);
+        float xInput = Input.GetAxis("Horizontal");
+        float zInput = Input.GetAxis("Vertical");
+
+
+        //calculate the direction we need move in
+        Vector3 dir = new Vector3(xInput, 0, zInput) * moveSpeed;
+        dir.y = rig.velocity.y;
+
+        rig.velocity = dir;
+
+
+
+        //set dir velocity
+        Vector3 facingDir = new Vector3(xInput, 0, zInput);
+
+        // are we holding down our movement buttons?
+        if (facingDir.magnitude > 0)
+        {
+            transform.forward = facingDir;
+        }
+
     }
-    public void GameOver()
+
+    void TryJump()
     {
-        GameUI.instance.SetEndScreen(false);
-        Time.timeScale = 0.0f;
+
+        Ray ray1 = new Ray(transform.position + new Vector3(0.5f, 0, 0.5f), Vector3.down);
+        Ray ray2 = new Ray(transform.position + new Vector3(-0.5f, 0, 0.5f), Vector3.down);
+        Ray ray3 = new Ray(transform.position + new Vector3(0.5f, 0, -0.5f), Vector3.down);
+        Ray ray4 = new Ray(transform.position + new Vector3(-0.5f, 0, -0.5f), Vector3.down);
+
+        bool cast1 = Physics.Raycast(ray1, 0.7f);
+        bool cast2 = Physics.Raycast(ray2, 0.7f);
+        bool cast3 = Physics.Raycast(ray3, 0.7f);
+        bool cast4 = Physics.Raycast(ray4, 0.7f);
+
+        if (cast1 || cast2 || cast3 || cast4)
+        {
+            rig.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            GameManager.instance.GameOver();
+
+        }
+        else if (other.CompareTag("Coin"))
+        {
+            GameManager.instance.AddScore(1);
+            Destroy(other.gameObject);
+            audioSource.Play();
+        }
+
+        //si tocamos Goal
+        else if (other.CompareTag("Goal"))
+        {
+            GameManager.instance.LevelEnd();
+        }
     }
 }
