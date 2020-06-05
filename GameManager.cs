42fb5a51e209a35039bcfb0c2f99f3010814bafe
Niruiz <43117479+Niruiz@users.noreply.github.com>
2020-06-05 15:16:07 +0200

    Funciona tecla pausa

--- GameManager.cs
+++ GameManager.cs
@@ -3,93 +3,81 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
-public class PlayerController : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
-    public float moveSpeed;
-    public float jumpForce;
-    private Rigidbody rig;
-    private AudioSource audioSource;
+    public int score;
+
+    public bool paused;
+
+    // instance
+    public static GameManager instance;
+
+void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            TogglePauseGame();
+        }
+    }
 
     void Awake()
     {
-        // get the components
-        rig = GetComponent<Rigidbody>();
-        audioSource = GetComponent<AudioSource>();
-    }
-
-
-    void Update()
-    {
-        Move();
-
-        if (Input.GetButtonDown("Jump"))
+        if (instance != null && instance != this)
         {
-            TryJump();
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
         }
     }
 
-    void Move()
+    // pauses or unpauses the game
+    public void TogglePauseGame()
     {
-        float xInput = Input.GetAxis("Horizontal");
-        float zInput = Input.GetAxis("Vertical");
+        paused = !paused;
 
+        if (paused)
+            Time.timeScale = 0.0f;
+        else
+            Time.timeScale = 1.0f;
 
-        //calculate the direction we need move in
-        Vector3 dir = new Vector3(xInput, 0, zInput) * moveSpeed;
-        dir.y = rig.velocity.y;
-
-        rig.velocity = dir;
-
-
-
-        //set dir velocity
-        Vector3 facingDir = new Vector3(xInput, 0, zInput);
-
-        // are we holding down our movement buttons?
-        if (facingDir.magnitude > 0)
-        {
-            transform.forward = facingDir;
-        }
-
+        GameUI.instance.TogglePauseScreen(paused);
     }
 
-    void TryJump()
+    public void AddScore(int scoreToGive)
     {
+        score += scoreToGive;
+        GameUI.instance.UpdateScoreText();
+    }
 
-        Ray ray1 = new Ray(transform.position + new Vector3(0.5f, 0, 0.5f), Vector3.down);
-        Ray ray2 = new Ray(transform.position + new Vector3(-0.5f, 0, 0.5f), Vector3.down);
-        Ray ray3 = new Ray(transform.position + new Vector3(0.5f, 0, -0.5f), Vector3.down);
-        Ray ray4 = new Ray(transform.position + new Vector3(-0.5f, 0, -0.5f), Vector3.down);
-
-        bool cast1 = Physics.Raycast(ray1, 0.7f);
-        bool cast2 = Physics.Raycast(ray2, 0.7f);
-        bool cast3 = Physics.Raycast(ray3, 0.7f);
-        bool cast4 = Physics.Raycast(ray4, 0.7f);
-
-        if (cast1 || cast2 || cast3 || cast4)
+    public void ResetScore()
+    {
+        score = 0;
+        GameUI.instance.UpdateScoreText();
+    }
+    public void LevelEnd()
+    {
+        // is this the last level?
+        if (SceneManager.sceneCountInBuildSettings == SceneManager.GetActiveScene().buildIndex + 1)
         {
-            rig.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            // display the win screen
+            WinGame();
+        }
+        else
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
-    void OnTriggerEnter(Collider other)
+    
+    public void WinGame()
     {
-        if (other.CompareTag("Enemy"))
-        {
-            GameManager.instance.GameOver();
-
-        }
-        else if (other.CompareTag("Coin"))
-        {
-            GameManager.instance.AddScore(1);
-            Destroy(other.gameObject);
-            audioSource.Play();
-        }
-
-        //si tocamos Goal
-        else if (other.CompareTag("Goal"))
-        {
-            GameManager.instance.LevelEnd();
-        }
+        GameUI.instance.SetEndScreen(true);
+    }
+    public void GameOver()
+    {
+        GameUI.instance.SetEndScreen(false);
+        Time.timeScale = 0.0f;
     }
 }
